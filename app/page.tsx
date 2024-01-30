b76736fcabc6402d799971dde3d973a558a20b49
gerdazk <gerda.zykute@gmail.com>
2024-01-30 19:05:21 +0200

    Add tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
 'use client'
 
-import MyComponent from '@/src/components/WebSocketTest'
+import MyComponent from '@/src/components/trash/WebSocketTest'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <MyComponent />
+        {/* <MyComponent /> */}
       </div>
     </main>
   )
---
+++ src/components/trash/WebSocketTest.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import useWebSocket from "../../utils/useWebSocket";
+import { ScoreBox } from "./ScoreBox";
+
+function MyComponent() {
+const [data, setData] = useState('')
+  useWebSocket({setData});
+
+  return (
+    <div>
+          <ScoreBox statistics={data && JSON.parse(data)} />
+    </div>
+  );
+}
+
+export default MyComponent;
