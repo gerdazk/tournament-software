3d81e0409b8180efd4bd67b6836af3d178c18aab
gerdazk <gerda.zykute@gmail.com>
2024-03-26 19:00:55 +0200

    Add type to simple card

--- src/components/SimpleCard.tsx
+++ src/components/SimpleCard.tsx
@@ -1,7 +1,13 @@
-import { CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
+import { CardDescription, CardHeader } from "@/components/ui/card"
 import { Label } from "@/components/ui/label"
 
-export const SimpleCard = ({title, subtitle, Icon}) => {
+type SimpleCardProps = {
+    title: string
+    subtitle: string
+    Icon?: React.FC
+}
+
+export const SimpleCard = ({title, subtitle, Icon}: SimpleCardProps) => {
     return (
         <CardHeader className="flex flex-row items-center p-0">
         {Icon && <Icon />}
