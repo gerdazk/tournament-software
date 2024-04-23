d056aa482600d989e4364b1ad94f78d3daaebfc3
gerdazk <gerda.zykute@gmail.com>
2024-04-23 23:23:47 +0300

    Update match display

--- app/admin/tournaments/[tournamentId]/matches/components/MatchesTable.tsx
+++ app/admin/tournaments/[tournamentId]/matches/components/MatchesTable.tsx
@@ -1,13 +1,13 @@
 import {
   Table,
   TableBody,
-  TableCaption,
   TableCell,
   TableHead,
   TableHeader,
   TableRow
 } from '@/components/ui/table'
 import { Match, Participant } from '@prisma/client'
+import { CheckIcon, ClockIcon } from 'lucide-react'
 
 type MatchesTableProps = {
   matches: (Match & {
@@ -15,26 +15,37 @@ type MatchesTableProps = {
   })[]
 }
 
-export const MatchesTable: React.FC<MatchesTableProps> = ({
-  matches = []
-}) => {
+export const MatchesTable: React.FC<MatchesTableProps> = ({ matches = [] }) => {
+  console.log({ matches })
   return (
     <Table className={`w-full`}>
       <TableHeader>
         <TableRow>
+          <TableHead className="w-[100px]"></TableHead>
           <TableHead className="w-[100px]">Player 1</TableHead>
           <TableHead>Player 2</TableHead>
           <TableHead>Score</TableHead>
         </TableRow>
       </TableHeader>
       <TableBody>
-        {matches.map(({ participants, score, id }) =>
+        {matches.map(({ participants, score, id, winnerId }) =>
           participants?.length ? (
             <TableRow key={id} className="cursor-pointer">
               <TableCell className="font-medium">
+                {score ? (
+                  <CheckIcon className="w-3 h-3" />
+                ) : (
+                  <ClockIcon className="w-3 h-3" />
+                )}
+              </TableCell>
+              <TableCell
+                className={`font-medium ${winnerId === participants?.[0]?.id ? `text-primary font-bold` : `text-muted-foreground`}`}
+              >
                 {participants?.[0]?.user?.name}
               </TableCell>
-              <TableCell className="font-medium">
+              <TableCell
+                className={`font-medium ${winnerId === participants?.[1]?.id ? `text-primary font-bold` : `text-muted-foreground`}`}
+              >
                 {participants?.[1]?.user?.name}
               </TableCell>
               <TableCell className="font-medium">{score}</TableCell>
